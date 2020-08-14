ef82f8902d1f32aeea6281d19a5429545bd4fe48
code-veronica <decodeveronica@gmail.com>
2020-08-14 16:44:43 -0300

    Updated on August 14th. Conditionals added.

--- Objects.js
+++ Objects.js
@@ -1,6 +1,13 @@
 let student = new Object()
-student.name = [ "Magalee", "Muriel", "Gaia" ]
-student.grade = [85, 78, 70]
+student.name = "Magali"
+student.grade = 85
 
-student.approved = true, 
-console.log(student)
+if (student.grade >= 70) {
+    console.log(student.name + " is approved.")
+}
+else if (student.grade  >= 60 && student.grade <= 69) {
+    console.log(student.name + " needs academic support.")
+}
+else {
+        console.log(student.name + " failed.")
+}
